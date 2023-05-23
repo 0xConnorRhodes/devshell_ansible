0c39ffdb9b8ea8ecd29b87d4e79f28825fa341cc
Connor Rhodes <connor@rhodes.contact>
2023-05-23 00:00:32 +0000

    added command to install nix to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,6 @@ sudo dnf install openssh-server
 sudo systemctl enable --now sshd
 ssh-keygen -a 100 -t ed25519
 cat ~/.ssh/id_ed25519.pub >> ~/.ssh/authorized_keys
+
+# install nix
+sh <(curl -L https://nixos.org/nix/install) --daemon
