4bea8737c1469f786fac7cce522e8072706087b7
Connor Rhodes <connor@rhodes.contact>
2023-05-25 14:43:00 -0500

    added new bootstrap commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-sudo dnf install openssh-server ansible
-ansible-galaxy install -r requirements.yaml
-sudo systemctl enable --now sshd
+# initial packages
+sudo dnf upgrade
+sudo dnf install openssh-server ansible git
+
+# set up ssh
 ssh-keygen -a 100 -t ed25519
 cat ~/.ssh/id_ed25519.pub >> ~/.ssh/authorized_keys
+sudo systemctl enable --now sshd
+
+# ansible deps
+ansible-galaxy install -r requirements.yaml
 
 # install nix
 sh <(curl -L https://nixos.org/nix/install) --daemon
